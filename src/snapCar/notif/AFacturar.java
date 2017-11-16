08b3031800226395217fa8327093bfb63459bde4
Andres Galaz <andres.galaz@gmail.com>
2017-11-16 11:15:42 -0300

    Se corrige mail a_facturar_02

--- src/snapCar/notif/AFacturar.java
+++ src/snapCar/notif/AFacturar.java
@@ -5,13 +5,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.apache.log4j.Logger;
 
 import prg.glz.FrameworkException;
+import prg.util.cnv.ConvertDate;
+import prg.util.cnv.ConvertException;
 import snapCar.net.CallWsMail;
 
 /**
@@ -37,6 +42,11 @@ public class AFacturar {
 
     @SuppressWarnings({ "rawtypes" })
     public void procesa() {
+        Locale.setDefault( new Locale ( "es" , "ES" ) );
+        SimpleDateFormat fmtLargo = new SimpleDateFormat( "EEEE d 'de' MMMM 'de' YYYY " );
+        SimpleDateFormat fmtDia = new SimpleDateFormat( "EEEE" );
+        SimpleDateFormat fmtSimple = new SimpleDateFormat( "dd/MM/YYY" );
+        
         try {
             {
                 // Crea tabla temporal wMemoryCierreTransf
@@ -45,9 +55,11 @@ public class AFacturar {
                 call.close();
             }
             String cSql = "SELECT w.cPatente \n"
-                    + ", DATE_FORMAT(w.dProximoCierre + INTERVAL -1 MONTH, '%d/%m/%Y')    dInicio \n"
-                    + ", DATE_FORMAT(w.dProximoCierre                    , '%d/%m/%Y')    dFin \n"
-                    + ", u.cEmail, u.cNombre                                              cNombre \n"
+                    // + ", DATE_FORMAT(w.dProximoCierre + INTERVAL -1 MONTH, '%d/%m/%Y')    dInicio \n"
+                    // + ", DATE_FORMAT(w.dProximoCierre                    , '%d/%m/%Y')    dFin \n"
+                    + ", w.dProximoCierre + INTERVAL -1 MONTH   dInicio \n"
+                    + ", w.dProximoCierre                       dFin \n"
+                    + ", u.cEmail, u.cNombre                    cNombre \n"
                     + ", DATEDIFF( DATE(NOW()) \n"
                     + "          , GREATEST( IFNULL(DATE( w.tUltTransferencia), '0000-00-00') \n"
                     + "                    , IFNULL(DATE( w.tUltViaje        ), '0000-00-00') \n"
@@ -70,10 +82,16 @@ public class AFacturar {
 
                 List<Map> to = callMail.createAddressTo( cNombre, cEmail );
 
+                Date dInicio = ConvertDate.toDate( rsNotif.getDate( "dInicio" ) );
+                Date dFin = ConvertDate.toDate( rsNotif.getDate( "dFin" ) );
+
+                
                 Map<String, String> mReg = new HashMap<String, String>();
                 mReg.put( "cPatente", cPatente );
-                mReg.put( "dInicio", rsNotif.getString( "dInicio" ) );
-                mReg.put( "dFin", rsNotif.getString( "dFin" ) );
+                mReg.put( "dInicio", fmtSimple.format( dInicio) );
+                mReg.put( "dFin", fmtSimple.format( dFin ) );
+                mReg.put( "cFecCierre", fmtLargo.format( dFin ) );
+                mReg.put( "cDiaCierre", fmtDia.format( dFin ) );
                 mReg.put( "cNombre", cPrimerNombre );
                 mReg.put( "nDiasNoSincro", String.valueOf( nDiasNoSincro ) );
 
@@ -89,6 +107,9 @@ public class AFacturar {
             }
             rsNotif.close();
             psSql.close();
+        } catch (ConvertException e) {
+            logger.error( "Al convertir fechas de inicio y fin", e );
+            throw new RuntimeException( e );
         } catch (SQLException e) {
             logger.error( "Al lee notificaciones tipo 2", e );
             throw new RuntimeException( e );
